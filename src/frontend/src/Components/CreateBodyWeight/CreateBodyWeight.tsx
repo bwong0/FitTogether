7a41311cb0c79c257b6d6774cfe5ecd190f35d83
billy wong <billywonger@gmail.com>
2023-08-28 22:03:44 -0700

    commit

--- src/frontend/src/Components/CreateBodyWeight/CreateBodyWeight.tsx
+++ src/frontend/src/Components/CreateBodyWeight/CreateBodyWeight.tsx
@@ -3,25 +3,50 @@ import "./CreateBodyWeight.css";
 import { postUserBodyWeightUrl } from "../../Resources/Url";
 import axios from "axios";
 const CreateBodyWeight = () => {
-  const [userId, setUserId] = useState<number|null>()
-  const [bodyWeight, setBodyWeight] = useState<number|null>();
+  const [userId, setUserId] = useState<number | null>();
+  const [bodyWeight, setBodyWeight] = useState<number | null>();
   const submitBodyWeightEntry = () => {
     const payload = { bodyWeight: bodyWeight };
-    console.log(userId)
-    console.log(bodyWeight)
-    // axios.post(postUserBodyWeightUrl(userIdtValue),payload);
-    
+    axios.post(postUserBodyWeightUrl(userId), payload).then((res) => {
+      console.log(res);
+    });
   };
-
+  const handleUserIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputUserId = event.target.value;
+    const newValue = parseFloat(inputUserId);
+    setUserId(isNaN(newValue) ? null : newValue);
+  };
+  const handleBodyWeightChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const inputUserId = event.target.value;
+    const newValue = parseFloat(inputUserId);
+    setBodyWeight(isNaN(newValue) ? null : newValue);
+  };
   return (
     <div className="CreateBodyWeight-Container">
       <form>
         <label>User Id</label>
-        <input type="number" value={userId == null ? "" : userId} onChange={(e) => setUserId(parseInt(e.target.value))} required />
+        <input
+          type="number"
+          value={userId == null ? "" : userId}
+          onChange={(e) => handleUserIdChange(e)}
+          required
+        />
         <label>BodyWeight Input</label>
-        <input type="number" value={bodyWeight == null ? "" : bodyWeight} onChange={(e) => setBodyWeight(parseFloat(e.target.value))} step={0.01} required />
-        <button type="button" onClick={submitBodyWeightEntry}>Add Entry</button>
+        <input
+          type="number"
+          value={bodyWeight == null ? "" : bodyWeight}
+          onChange={(e) => handleBodyWeightChange(e)}
+          step={0.01}
+          required
+        />
+        <button type="submit" onClick={submitBodyWeightEntry}>
+          Add Entry
+        </button>
       </form>
+      <p> {userId} </p>
+      <p> {bodyWeight} </p>
     </div>
   );
 };
